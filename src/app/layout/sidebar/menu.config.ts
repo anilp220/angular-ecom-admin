aa589dd1d732550471cb0a00f52f5693573c3afa
anil patel <anilp220@gmail.com>
2026-02-07 18:58:35 +0530

    sidebar config

---
+++ src/app/layout/sidebar/menu.config.ts
@@ -0,0 +1,14 @@
+export const MENU_ITEMS = [
+
+  { label: 'Dashboard', icon: 'pi pi-home', route: '/dashboard' },
+
+  { label: 'Products', icon: 'pi pi-box', route: '/products' },
+
+  { label: 'Users', icon: 'pi pi-users', route: '/users' },
+
+  { label: 'Carts', icon: 'pi pi-shopping-cart', route: '/carts' },
+
+  { label: 'Orders', icon: 'pi pi-list', route: '/orders' },
+
+  { label: 'Settings', icon: 'pi pi-cog', route: '/settings' }
+];
--- src/app/layout/sidebar/sidebar.ts
+++ src/app/layout/sidebar/sidebar.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { MENU_ITEMS } from './menu.config';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [RouterModule,CommonModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
 })
 export class Sidebar {
-
+  menu = MENU_ITEMS;
 }

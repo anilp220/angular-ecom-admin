d34bf2584eae943c58b7a035b41eeb6c20bb74da
anil patel <anilp220@gmail.com>
2026-02-08 15:55:40 +0530

    product table and pagination

--- src/app/core/services/products.service.ts
+++ src/app/core/services/products.service.ts
@@ -1,8 +1,36 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from '../models/product.model';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class Products {
-  
+@Injectable({ providedIn: 'root' })
+export class ProductsService {
+
+  private http = inject(HttpClient);
+
+  private base =
+    'https://dummyjson.com/products';
+
+  // ---------- LIST ----------
+
+  getProducts(
+    limit = 10,
+    skip = 0
+  ) {
+    return this.http.get<Product[]>(
+      `${this.base}?limit=${limit}&skip=${skip}`
+    );
+  }
+
+  // ---------- SEARCH ----------
+
+  searchProducts(
+    query: string,
+    limit = 10,
+    skip = 0
+  ) {
+    return this.http.get<any>(
+      `${this.base}/search?q=${query}&limit=${limit}&skip=${skip}`
+    );
+  }
+
 }
--- src/app/features/products/list/list.component.ts
+++ src/app/features/products/list/list.component.ts
@@ -1,11 +1,82 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ProductsService } from '../../../core/services/products.service';
+import { Product } from '../../../core/models/product.model';
 
 @Component({
   selector: 'app-list',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss'],
+  standalone: true
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
+  private productService = inject(ProductsService);
 
+  products = signal<Product[]>([]);
+  loading = signal(true);
+  total = signal(0);
+  isSearched = signal(false);
+  currentPage = signal(1);
+
+  page = 1;
+  limit = 10;
+  search = '';
+  pageCount = 1;
+
+  ngOnInit() {
+    this.loadProducts();
+  }
+
+  loadProducts() {
+    this.loading.set(true);
+    
+    const skip = (this.page - 1) * this.limit;
+    this.currentPage.set(this.page);
+
+    const apiCall = this.search 
+      ? this.productService.searchProducts(this.search, this.limit, skip) 
+      : this.productService.getProducts(this.limit, skip);
+
+    apiCall.subscribe({
+      next: (res) => {
+        this.products.set(res.products);
+        this.total.set(res.total);
+        this.loading.set(false);
+        this.pageCount = Math.ceil(res.total / this.limit);
+      },
+      error: () => {
+        this.loading.set(false);
+      }
+    });
+  }
+
+  onResetSearch(){
+    this.search = '';
+    this.loadProducts();
+    this.isSearched.set(false);
+  }
+
+  nextPage(){
+    this.page++;
+    this.loadProducts();
+  }
+
+  prevPage(){
+    if(this.page == 1) return;
+    this.page--;
+    this.loadProducts();
+  }
+
+  onSearch(){
+    this.page = 1;
+    this.loadProducts();
+    this.isSearched.set(true);
+  }
+
+  goToPage(page: number) {
+    this.page = page;
+    this.loadProducts();
+  }
 }
